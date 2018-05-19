da82aebacf274ef4583bf7715fa145eee5146a1b
Alex Nichol <unixpickle@gmail.com>
2018-05-19 13:25:52 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""
+Package meta-data.
+"""
+
+from setuptools import setup
+
+setup(
+    name='data-balance',
+    version='0.0.1',
+    description='Experiments with data balancing.',
+    url='https://github.com/unixpickle/data-balance',
+    author='Alex Nichol',
+    author_email='unixpickle@gmail.com',
+    license='MIT',
+    packages=['data_balance'],
+    install_requires=['Pillow', 'numpy']
+)
