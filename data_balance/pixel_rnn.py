8e19c443b58508ba3bf2c9805daf4b3f154e4218
Alex Nichol <unixpickle@gmail.com>
2018-05-20 12:06:34 -0700

    add pixel RNN density model
    I was hoping this would be better than the VAE, but it's actually much worse. Under a row-based RNN, 8 had like 0.9999% of the density.
    
    Squashed commit of the following:
    
    commit b013269ffe178ed3166b0b2ccb2e1417260b4ed7
    Author: Alex Nichol <unixpickle@gmail.com>
    Date:   Sun May 20 12:01:42 2018 -0700
    
        fix crash
    
    commit 2061f665cc28c1e5dca2089ca6caf5193c5c7f40
    Author: Alex Nichol <unixpickle@gmail.com>
    Date:   Sun May 20 12:00:41 2018 -0700
    
        more autoregression!
    
    commit 74d9772c0ce0c7f5740b44f4fad21cd29ed4d292
    Author: Alex Nichol <unixpickle@gmail.com>
    Date:   Sun May 20 11:58:08 2018 -0700
    
        fix rnn naming crash
    
    commit 760ceb9d33a66ee20d749f34fc2418cb82dca2df
    Author: Alex Nichol <unixpickle@gmail.com>
    Date:   Sun May 20 11:57:23 2018 -0700
    
        fix rnn balance crash
    
    commit b4ed5645ecfe7e142979a15416c758d0608814e7
    Author: Alex Nichol <unixpickle@gmail.com>
    Date:   Sun May 20 11:56:01 2018 -0700
    
        fix another rnn sampling bug
    
    commit 576197adb9f9071a3862346f9a4a3f3a6c332e5b
    Author: Alex Nichol <unixpickle@gmail.com>
    Date:   Sun May 20 11:55:26 2018 -0700
    
        fix rnn sampling dtypes
    
    commit 1a033f8cdb8e445bbfa13707188a5c4eec5a4231
    Author: Alex Nichol <unixpickle@gmail.com>
    Date:   Sun May 20 11:52:21 2018 -0700
    
        fix rnn dtypes
    
    commit 84cb88d039eb2c81b2431c2c9fcb7aa08d4428ee
    Author: Alex Nichol <unixpickle@gmail.com>
    Date:   Sun May 20 11:50:25 2018 -0700
    
        fix undefined variable
    
    commit 60608533cf9a07ec2ab49205065e5f93933607e7
    Author: Alex Nichol <unixpickle@gmail.com>
    Date:   Sun May 20 11:49:37 2018 -0700
    
        fix pixel RNN sampling stuff
    
    commit 64213b3796292e553b4a42b310e6b9e4a39c23c5
    Author: Alex Nichol <unixpickle@gmail.com>
    Date:   Sun May 20 11:40:08 2018 -0700
    
        drafted pixel RNN

---
+++ data_balance/pixel_rnn.py
@@ -0,0 +1,93 @@
+"""
+A pixel RNN density model.
+"""
+
+import os
+
+import numpy as np
+import tensorflow as tf
+
+
+CHUNK_SIZE = 8
+TIMESTEPS = (28 * 28) // CHUNK_SIZE
+
+
+def rnn_log_probs_tf(inputs):
+    """
+    Get the log probability of the input images.
+
+    Args:
+      inputs: a bool [batch x 28 x 28 x 1] Tensor.
+
+    Returns:
+      A batch of log probabilities.
+    """
+    seqs = tf.cast(tf.reshape(inputs, [-1, TIMESTEPS, CHUNK_SIZE]), tf.float32)
+    shifted = tf.concat([tf.zeros_like(seqs[:, :1]), seqs[:, :-1]], axis=1)
+    rnn = _make_rnn()
+    out_layer = _make_out_layer()
+    states = rnn.zero_state(tf.shape(inputs)[0], tf.float32)
+    logits = []
+    for i in range(TIMESTEPS):
+        features, states = rnn(shifted[:, i], states)
+        logits.append(out_layer(features))
+    logits = tf.stack(logits, axis=1)
+    loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=seqs, logits=logits)
+    return tf.reduce_sum(loss, axis=[1, 2])
+
+
+def rnn_sample(batch_size):
+    """
+    Create a batch of samples from the model.
+
+    Returns:
+      A [batch x 28 x 28 x 1] Tensor.
+    """
+    rnn = _make_rnn()
+    out_layer = _make_out_layer()
+    inputs = tf.zeros([batch_size, CHUNK_SIZE], dtype=tf.bool)
+    states = rnn.zero_state(batch_size, tf.float32)
+    result = []
+    for _ in range(TIMESTEPS):
+        features, states = rnn(tf.cast(inputs, tf.float32), states)
+        logits = out_layer(features)
+        inputs = tf.distributions.Bernoulli(logits=logits).sample()
+        result.append(inputs)
+    return tf.reshape(tf.stack(result, axis=1), [-1, 28, 28, 1])
+
+
+def rnn_log_probs_np(images, checkpoint='pixel_rnn_checkpoint', batch=64):
+    """
+    Like rnn_log_probs_tf, but takes a numpy array and
+    produces a numpy array.
+    """
+    with tf.Graph().as_default():
+        image_ph = tf.placeholder(tf.float32, shape=(None, 28, 28, 1))
+        with tf.variable_scope('pixel_rnn'):
+            log_probs = rnn_log_probs_tf(image_ph)
+        saver = tf.train.Saver()
+        all_probs = []
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            saver.restore(sess, checkpoint_name(checkpoint))
+            for i in range(0, len(images), batch):
+                if i + batch > len(images):
+                    batch_images = images[i:]
+                else:
+                    batch_images = images[i: i + batch]
+                all_probs.extend(sess.run(log_probs, feed_dict={image_ph: batch_images}))
+    return np.array(all_probs)
+
+
+def checkpoint_name(dir_name):
+    return os.path.join(dir_name, 'model.ckpt')
+
+
+def _make_rnn():
+    return tf.nn.rnn_cell.LSTMCell(num_units=128, use_peepholes=True)
+
+
+def _make_out_layer():
+    weights = tf.get_variable('weights', dtype=tf.float32, shape=[128, CHUNK_SIZE])
+    biases = tf.get_variable('biases', dtype=tf.float32, shape=[CHUNK_SIZE])
+    return lambda x: tf.matmul(x, weights) + biases
